f1b416b6297c4e6f7bb857cdd115bb72e5bee8fc
bmpq <73074311+bmpq@users.noreply.github.com>
2023-03-26 18:12:59 +0300

    Update ui.py
    clearer interface

--- ui.py
+++ ui.py
@@ -24,32 +24,14 @@ class STRA_PT_Panel(Panel):
         col_joints = col_selected.children.get(col_selected.name + '_joints')
         generated = col_joints is not None and len(col_joints.objects) > 0
 
-        r = layout.row()
-        r.operator("stra.viewport_collider_hide",
-                   icon='HIDE_ON' if props_viewport.hide else 'HIDE_OFF', text='')
-        r.operator("stra.viewport_collider_selectable",
-                   icon='RESTRICT_SELECT_OFF' if props_viewport.selectable else 'RESTRICT_SELECT_ON', text='')
-        r.operator("stra.viewport_collider_show_in_front",
-                   icon='XRAY' if props_viewport.show_in_front else 'CUBE', text='')
-        r.operator("stra.viewport_collider_detect", icon='ALIGN_LEFT', text='')
-
-        layout.separator(factor=2)
-
         if col_selected == context.scene.collection:
             layout.label(text='Scene collection selected')
             return
 
-        if generated:
-            layout.label(text=f'{len(col_joints.objects)} generated joint constraints')
-        layout.prop(props_joint, "type", text="Constraint type")
-        if props_joint.type == 'GENERIC':
-            r = layout.row()
-            r.prop(props_joint, "leeway_linear")
-            r.prop(props_joint, "leeway_angular")
-        layout.prop(props_joint, "use_local_collisions")
-        layout.prop(props_joint, "break_threshold")
-
-        layout.separator(factor=2)
+        r = layout.row()
+        r.alignment = 'CENTER'
+        r.active = False
+        r.label(text='-- Joint generation --')
 
         mesh_amount = 0
         for ob in col_selected.objects:
@@ -61,18 +43,27 @@ class STRA_PT_Panel(Panel):
 
         layout.label(text=f'{mesh_amount} mesh objects in [{col_selected.name}]')
 
+        if generated:
+            layout.label(text=f'{len(col_joints.objects)} generated joint constraints')
+        layout.prop(props_joint, "type", text="Constraint type")
+        if props_joint.type == 'GENERIC':
+            r = layout.row()
+            r.prop(props_joint, "leeway_linear")
+            r.prop(props_joint, "leeway_angular")
+        layout.prop(props_joint, "use_local_collisions")
+        layout.prop(props_joint, "break_threshold")
+
         layout.prop(props_structure, "use_overlap_margin")
         if props_structure.use_overlap_margin:
             layout.prop(props_structure, "overlap_margin")
         layout.prop(props_structure, "subd")
 
+        r = layout.row()
+        r.scale_y = 2
         if props_structure.progress > 0.0 and props_structure.progress < 1.0:
-            layout.label(text=f"Progress: {props_structure.progress*100:.2f}%")
+            r.label(text=f"Progress: {props_structure.progress*100:.2f}%")
         else:
-            layout.operator("stra.structure_generate", icon='MOD_MESHDEFORM')
-
-        if len(context.selected_objects) == 0:
-            return
+            r.operator("stra.structure_generate", icon='MOD_MESHDEFORM')
 
         layout.separator(factor=2)
 
@@ -94,10 +85,25 @@ class STRA_PT_Panel(Panel):
                         sh_amount[sh] += 1
                         break
 
+        r = layout.row()
+        r.alignment = 'CENTER'
+        r.active = False
+        r.label(text='-- Collider generation --')
+
+        b = layout.box()
+        r = b.row()
+        r.operator("stra.viewport_collider_hide",
+                   icon='HIDE_ON' if props_viewport.hide else 'HIDE_OFF', text='')
+        r.operator("stra.viewport_collider_selectable",
+                   icon='RESTRICT_SELECT_OFF' if props_viewport.selectable else 'RESTRICT_SELECT_ON', text='')
+        r.operator("stra.viewport_collider_show_in_front",
+                   icon='XRAY' if props_viewport.show_in_front else 'CUBE', text='')
+        r.operator("stra.viewport_collider_detect", icon='ALIGN_LEFT', text='')
+
+        layout.label(text=f'{mesh_amount} mesh selected ({rb_amount} RB):')
+
         if mesh_amount == 0:
             return
-
-        layout.label(text=f'{mesh_amount} mesh selected ({rb_amount} RB)')
 
         b = layout.box()
         for sh in sh_amount:
@@ -118,7 +124,9 @@ class STRA_PT_Panel(Panel):
             layout.prop(props_collider, "scale")
             layout.prop(props_collider, "voxel_size")
 
+        r = layout.row()
+        r.scale_y = 2
         if props_collider.progress > 0.0 and props_collider.progress < 1.0:
-            layout.label(text=f"Progress: {props_collider.progress*100:.2f}%")
+            r.label(text=f"Progress: {props_collider.progress*100:.2f}%")
         else:
-            layout.operator("stra.collider_generate", icon='RIGID_BODY')
+            r.operator("stra.collider_generate", icon='MESH_ICOSPHERE')
