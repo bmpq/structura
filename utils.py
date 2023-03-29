034362165cc5a17ef5a984461519760a1cbfbab2
bmpq <73074311+bmpq@users.noreply.github.com>
2023-03-29 18:27:52 +0300

    proper collection purging

--- utils.py
+++ utils.py
@@ -2,25 +2,21 @@ from gc import collect
 import bpy
 
 
+def clear_collection(col_clear):
+    for child_col in col_clear.children:
+        clear_collection(child_col)
+    for ob in col_clear.objects:
+        for col in ob.users_collection:
+            col.objects.unlink(ob)
+
+
 def reset_collection(parent_collection, name):
-    col_reset = None
-    for col in bpy.data.collections:
-        if col.name == name:
-            col_reset = col
-        if 'RigidBodyWorld' in col.name:
-            colrbw = col
-        if 'RigidBodyConstraints' in col.name:
-            colrbc = col
+    col_reset = bpy.data.collections.get(name)
     if col_reset is None:
         col_reset = bpy.data.collections.new(name)
         parent_collection.children.link(col_reset)
     else:
-        for ob in col_reset.objects:
-            if colrbw.objects.get(ob.name) is not None:
-                colrbw.objects.unlink(ob)
-            if colrbc.objects.get(ob.name) is not None:
-                colrbc.objects.unlink(ob)
-            col_reset.objects.unlink(ob)
+        clear_collection(col_reset)
 
     bpy.ops.outliner.orphans_purge(do_local_ids=True)
 
